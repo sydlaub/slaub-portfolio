6eb6c879991c9cb2f7f8d363272d694c37d40dda
Sydney Laub <syd.laub18@gmail.com>
2023-08-31 11:52:15 -0400

    edits to sidebar styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,25 +3,29 @@ import './index.scss'
 // import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo-sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, 
+    faHome, 
+    faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+    faLinkedin,
+    faGithub,
+} from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
             {/* <img src={LogoS} alt="logo" />
             <img className= "sub-logo" src={LogoSubtitle} alt="slobodan" />   */}
             <h1>
                 SYDNEY ROSE LAUB
             </h1>
-        </Link>
 
         <nav>
             <NavLink 
             exact="true" 
             activeclassname="active" 
             to="/">
-                <FontAwesomeIcon icon={faHome} color="#60656F" />
+                <FontAwesomeIcon icon={faHome} color="#3266AE" />
             </NavLink>
 
             <NavLink 
@@ -29,7 +33,7 @@ const Sidebar = () => (
             activeclassname="active" 
             className="about-link" 
             to="/about">
-                <FontAwesomeIcon icon={faUser} color="#60656F" />
+                <FontAwesomeIcon icon={faUser} color="#3266AE" />
             </NavLink>
 
             <NavLink 
@@ -37,9 +41,27 @@ const Sidebar = () => (
             activeclassname="active" 
             className="contact-link" 
             to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#60656F" />
+                <FontAwesomeIcon icon={faEnvelope} color="#3266AE" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://www.linkedin.com/in/sydney-laub-1b854919a/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#81A6DA" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel='noreferrer'
+                    href='https://github.com/sydlaub'>
+                    <FontAwesomeIcon icon={faGithub} color="#81A6DA" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
